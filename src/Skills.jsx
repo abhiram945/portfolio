987b77bb561079ed09ba95f3c0979da56b382bbe
abhiram945 <abhiramdev945@gmail.com>
2025-05-02 11:54:16 +0530

    added new skills

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -4,38 +4,41 @@ function Skills() {
     return (
         <div id='skills' className='skillsSectionContainer'>
             <h2 className=''>My <span className=''>SKILLS</span></h2>
-            <div className="rowsContainer">
-                <div className='row'>
-                    <div className='skillsContainer'>
-                        <p className='skillHeading'>Web Development</p>
-                        {["mongoDb", "express", "react", "node"].map((image, index) => <div className='skillContainer' key={index}>
-                            <img src={`/images/skills/${image}.png`} alt={image} className='' />
-                            <p className=''>{image.toUpperCase()}</p>
-                        </div>)}
-                    </div>
-                    <div className='skillsContainer'>
-                        <p className='skillHeading'>Programming</p>
-                        {["java"].map((image, index) => <div className='skillContainer' key={index}>
-                            <img src={`/images/skills/${image}.png`} alt={image} className='' />
-                            <p className=''>{image.toUpperCase()}</p>
-                        </div>)}
-                    </div>
+            <div className="skillsContainer">
+                <div className='skillContainer'>
+                    <p className='skillHeading'>Web Development</p>
+                    {["express", "react", "node","python-flask"].map((image, index) => <div className='skill' key={index}>
+                        <img src={`/images/skills/${image}.png`} alt={image} className='' />
+                        <p className=''>{image.toUpperCase()}</p>
+                    </div>)}
                 </div>
-                <div className='row'>
-                    <div className='skillsContainer'>
-                        <p className='skillHeading'>App Development</p>
-                        {["reactNative", "expo"].map((image, index) => <div className='skillContainer' key={index}>
-                            <img src={`/images/skills/${image}.png`} alt={image} className='' />
-                            <p className=''>{image.toUpperCase()}</p>
-                        </div>)}
-                    </div>
-                    <div className='skillsContainer'>
-                        <p className='skillHeading'>Tools & Platforms</p>
-                        {["git", "github"].map((image, index) => <div className='skillContainer' key={index}>
-                            <img src={`/images/skills/${image}.png`} alt={image} className='' />
-                            <p className=''>{image.toUpperCase()}</p>
-                        </div>)}
-                    </div>
+                <div className='skillContainer'>
+                    <p className='skillHeading'>Programming</p>
+                    {["java","python"].map((image, index) => <div className='skill' key={index}>
+                        <img src={`/images/skills/${image}.png`} alt={image} className='' />
+                        <p className=''>{image.toUpperCase()}</p>
+                    </div>)}
+                </div>
+                <div className='skillContainer'>
+                    <p className='skillHeading'>Database</p>
+                    {["mongoDb","sql"].map((image, index) => <div className='skill' key={index}>
+                        <img src={`/images/skills/${image}.png`} alt={image} className='' />
+                        <p className=''>{image.toUpperCase()}</p>
+                    </div>)}
+                </div>
+                <div className='skillContainer'>
+                    <p className='skillHeading'>App Development</p>
+                    {["reactNative", "expo"].map((image, index) => <div className='skill' key={index}>
+                        <img src={`/images/skills/${image}.png`} alt={image} className='' />
+                        <p className=''>{image.toUpperCase()}</p>
+                    </div>)}
+                </div>
+                <div className='skillContainer'>
+                    <p className='skillHeading'>Tools & Platforms</p>
+                    {["git", "github"].map((image, index) => <div className='skill' key={index}>
+                        <img src={`/images/skills/${image}.png`} alt={image} className='' />
+                        <p className=''>{image.toUpperCase()}</p>
+                    </div>)}
                 </div>
             </div>
         </div>
